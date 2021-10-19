968fe983262d7689a19122e9c39874c0b442ca0d
wireturn <89811485+wireturn@users.noreply.github.com>
2021-10-19 17:21:27 +0100

    Create mod.go

---
+++ mod.go
@@ -0,0 +1,56 @@
+module github.com/wireturn/bsv
+
+go 1.17
+
+require (
+	
+	// Bitcoin Schema
+	github.com/bitcoinschema/go-aip v0.1.7
+	github.com/bitcoinschema/go-b v0.0.12
+	github.com/bitcoinschema/go-bap v0.2.6
+	github.com/BitcoinSchema/go-bitcoin v0.3.19
+	github.com/bitcoinschema/go-bmap v0.0.27
+	github.com/bitcoinschema/go-bob v0.1.8
+	github.com/bitcoinschema/go-map v0.0.13
+
+	// Dotwallet
+	github.com/dotwallet/dotwallet-go-sdk v0.0.3
+	
+	// libsv
+	github.com/libsv/go-bt v2.0.0-beta.9
+	github.com/libsv/go-bk v0.1.4
+	github.com/libsv/go-bc v0.1.7
+	// github.com/libsv/go-p4 v0.0.6
+	github.com/libsv/payd v0.0.9
+	github.com/libsv/go-spvchannels
+	
+	// Mr.Z
+	github.com/mrz1836/go-mattercloud v0.1.5
+	github.com/mrz1836/go-polynym v0.4.5
+	github.com/mrz1836/go-preev v0.2.4
+	github.com/mrz1836/go-whatsonchain v0.6.5
+	github.com/mrz1836/paymail-inspector v0.3.13
+
+	// TonicPOW
+	github.com/tonicpow/go-bsvrates v0.1.12
+	github.com/tonicpow/go-handcash-connect v0.1.9
+	// github.com/tonicpow/go-moneybutton v0.0.2
+	github.com/tonicpow/go-minercraft v0.3.1
+	github.com/tonicpow/go-paymail v0.2.12
+	github.com/tonicpow/go-tonicpow v0.6.14
+	
+	// sCript
+	github.com/sCrypt-Inc/go-scryptlib
+
+	// Tokenized
+	github.com/tokenized/envelope v0.3.0
+	github.com/tokenized/spynode v0.1.1
+	github.com/tokenized/smart-contract v1.0.3
+
+	// Sensible
+	// github.com/sensible-contract/satomempool v1.0
+	// github.com/sensible-contract/satotx 
+	// github.com/sensible-contract/sensiblequery v1.3
+	// github.com/sensible-contract/sensibled v1.8.6
+
+)
